6be31038e8397ee5a6bbc66f1b85a59fd95fc67f
Clayton Phillipp <phillipp@Claytons-MacBook-Pro.local>
2011-12-11 23:42:19 -0700

    updated css

--- js/index.js
+++ js/index.js
@@ -2,16 +2,19 @@ $(document).ready(function() {
   $('#pocatello').click(function() {
     $('#pocatello ul').show('fast', function() {
       // Animation complete.
+      return false
     });
   });
   $('#idahoFalls').click(function() {
     $('#idahoFalls ul').show('fast', function() {
       // Animation complete.
+      return false
     });
   });
   $('#twinFalls').click(function() {
     $('#twinFalls ul').show('fast', function() {
       // Animation complete.
+      return false
     });
   });
   $('.nav').mouseleave(function() {
