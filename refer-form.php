3bed5ccfb60361f807afdb0e3df1c44dcf00ccf7
Clayton Phillipp <phillipp@Claytons-MacBook-Pro.local>
2011-12-16 21:02:37 -0700

    updated site files

--- refer-form.php
+++ refer-form.php
@@ -1,10 +1,13 @@
 <?php
     session_start();
     $to = "clayphillipp@gmail.com";
-    $subject = "Advertise Form";
-    $yourEmail = $_POST['yourEmail'];
-    $thereEmail = $_POST['thereEmail'];
-    $message = $yourEmail . "\n ". $thereEmail;
+    $subject = "Referral Form";
+    $email = $_POST['email'];
+    $address = $_POST['address'];
+    $message = $_POST['message'];
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $message = $name . "\n ". $email . "\n ". $phone . "\n ". $address;
     $headers = "From: $email";
     $sent = mail($to, $subject, $message, $headers);
     if($sent) {
